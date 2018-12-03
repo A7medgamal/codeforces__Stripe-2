0486b438f2c41bd66e891b34ccc1abf46862239a
A7medgamal <40647234+A7medgamal@users.noreply.github.com>
2018-12-03 23:47:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+/*if sum of element dont divisible by 3 the answer is zero else i add element by element
+and sum of them become equal the total sum divisible by 3
+this one solution and complete this until the sum of element become 2* total sum divisible by 3 this Indicates the stripe Can be divided
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long  ll;
 typedef bitset<15>mask;
 #define F first
 #define S second
-ll n,m,x,y,z,k,sol,sum,ans,l=-1e17,r,xx,yy,a[1000000],b[1000000],mem[200000][5];
+ll n,m,x,y,z,k,sol,sum,ans,l,r,xx,yy,a[1000000],b[1000000];
 vector<ll>v1;
 vector<ll>v2;
 pair<ll,pair<ll,ll> >pp[1000000];
@@ -12,7 +17,6 @@ pair<ll,ll>p[1000000];
 map<ll,ll>ma;
 string s1,s2,s;
 char c;
-
 int main()
 {
 //freopen("input.txt", "r", stdin);
@@ -20,65 +24,20 @@ int main()
     cin>>n;
     for(int i=0;i<n;i++)
         cin>>a[i],sum+=a[i];
-    if(abs(sum)%3==0)
+    if(sum%3!=0)
     {
-        y=sum/3;
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==0)
-            {
-                v1.push_back(0);
-                continue;
-            }
-            if(y>=0)
-            {
-                if(l>y)
-                {
-                    cout<<0<<endl;
-                    return 0;
-                }
-                else if(l<y)
-                {
-                    if(l==-1e17)
-                    l=a[i];
-                    else l+=a[i];
-                    if(l==y)
-                    {
-                        v1.push_back(l);
-                    l=-1e17;
-                    }
-                }
-            }
-            else
-            {
-                 if(l<y)
-                {
-                    cout<<0<<endl;
-                    return 0;
-                }
-                 else if(l>y)
-                {
-                    if(l==-1e17)
-                    l=a[i];
-                    else l+=a[i];
-                    if(l==y)
-                    {
-                        v1.push_back(l);
-                    l=-1e17;
-                    }
-                }
-            }
-        }
-    }
-    else {
-
         cout<<0<<endl;
         return 0;
     }
-    for(int i=0;i<v1.size();i++)
-      //  cout<<v1[i]<<" ";
-    memset(mem,-1,sizeof mem);
-    cout<<dp()<<endl;
+    sum/=3;
+    for(int i=0;i<n-1;i++)
+    {
+        x+=a[i];
+        if(x==2*sum)ans+=y;
+        if(x==sum)y++;
+    }
+    cout<<ans<<endl;
+
 
     return 0;
 }
